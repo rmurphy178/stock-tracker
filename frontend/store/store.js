553e7fcefee9d67f272334c1e2e01ad761ad3710
Ryan Murphy <ryan.murphy@alumni.stanford.edu>
2017-09-26 13:58:48 -0700

    setup and initial commit

---
+++ frontend/store/store.js
@@ -0,0 +1,16 @@
+import { compose, createStore, applyMiddleware } from 'redux';
+import { autoRehydrate, persistStore } from 'redux-persist';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import rootReducer from '../reducers/root_reducer';
+
+const middlewares = [thunk, logger];
+
+let store = compose(
+    applyMiddleware(...middlewares),
+    autoRehydrate()
+)(createStore)(rootReducer);
+
+persistStore(store);
+
+export default store;
